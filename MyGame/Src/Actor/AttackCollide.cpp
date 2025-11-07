05610fddc8dbc2af43404cfa5c9e0ef914df19d7
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-11-07 09:41:40 +0900

    Add:攻撃用当たり判定の実装

---
+++ MyGame/Src/Actor/AttackCollide.cpp
@@ -0,0 +1,52 @@
+#include "AttackCollide.h"
+#include "../Actor/Charactor.h"
+
+AttackCollide::AttackCollide(Charactor* owner, float radius, const GSvector3& pos, const GSvector3& offset)
+{
+	owner_ = owner;
+	tag_ = ActorName::ATKCollide;
+	//UŒ‚Žå‚Ì–¼‘O‚ð“o˜^
+	name_ = owner_->GetTag() + tag_;
+	collider_ = BoundingSphere(radius,pos);
+	
+	colliderOffset_ = offset;
+	collider_.Trigger(true);
+	collider_.Enable(false);
+}
+
+AttackCollide::~AttackCollide()
+{
+
+}
+
+void AttackCollide::IsAttack(float time, float attack)
+{
+	collider_.Enable(true);
+	duration_ = time;
+	attack_ = attack;
+}
+
+void AttackCollide::Update(float deltaTime)
+{
+	collider_.Position(transform_.position() + colliderOffset_);
+	if (!collider_.Enable()) return;
+	duration_ -= deltaTime / 60.0f;
+	if(collider_.Enable()) collider_.Enable(false);
+	if (duration_ <= 0) collider_.Enable(false);
+}
+
+void AttackCollide::Draw() const
+{
+#if _DEBUG
+	//ƒfƒoƒbƒN—p
+	collider_.Draw();
+#endif
+
+}
+void AttackCollide::React(Actor& other)
+{
+	Charactor* chara = dynamic_cast<Charactor*>(&other);
+	// CharactorˆÈŠO‚à‚µ‚­‚ÍUŒ‚Žå‚È‚çreturn
+	if (!chara || owner_ == chara) return;
+	chara->TakeDamage(attack_);
+}
---
+++ MyGame/Src/Actor/AttackCollide.h
@@ -0,0 +1,24 @@
+#ifndef ATTACK_COLLIDE_H_
+#define ATTACK_COLLIDE_H_
+#include "../Actor/Actor.h"
+
+class Charactor;
+
+class AttackCollide : public Actor
+{
+public:
+	AttackCollide(Charactor* owner ,float radius = 1.0f,const GSvector3& pos = { 0,0,0 },const GSvector3& offset = { 0,0,0 });
+	~AttackCollide();
+	void IsAttack(float time = 1.0f,float attack = 0.0f);
+	void Update(float deltTime)override;
+	void Draw()const override;
+	void React(Actor& other)override;
+private:
+	Charactor* owner_{ nullptr };
+	float attack_{ 1.0f };
+	bool isAttack{ false };
+	//çUåÇÇÃéùë±éûä‘
+	float duration_{ 1.0f };
+};
+
+#endif

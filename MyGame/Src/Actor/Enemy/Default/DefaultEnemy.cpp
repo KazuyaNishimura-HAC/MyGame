0a1f3741f9328dc656c89e89200bb680ad63eb42
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-11-07 09:46:22 +0900

    Update:エネミー基本動作実装

--- MyGame/Src/Actor/Enemy/Default/DefaultEnemy.cpp
+++ MyGame/Src/Actor/Enemy/Default/DefaultEnemy.cpp
@@ -7,19 +7,23 @@
 #include "../State/EnemyIdle.h"
 #include "../State/EnemyMove.h"
 #include "../State/EnemyAttack.h"
+#include "../State/EnemyDamage.h"
+#include "../State/EnemyDead.h"
 
 DefaultEnemy::DefaultEnemy(IWorld* world, const GSvector3& position, GSuint mesh)
     :Enemy(world, position, mesh)
 {
-    //初期座標設定
-    transform_.position(position);
+    name_ = ActorName::DefaultEnemy;
     //ステートの追加
     states_.AddState(EnemyState::Idle, new EnemyIdle(this));
     states_.AddState(EnemyState::Move, new EnemyMove(this));
     states_.AddState(EnemyState::Attack, new EnemyAttack(this));
+    states_.AddState(EnemyState::Damage, new EnemyDamage(this));
+    states_.AddState(EnemyState::Dead, new EnemyDead(this));
     states_.ChangeState(EnemyState::Idle);
-    collider_ = BoundingSphere(1.0f);
-    colliderOffset_ = { 0.0f,1.0f,0.0f };
+    collider_ = BoundingSphere(1);
+    //攻撃処理
+    mesh_->AddEvent(2, 30, [=] {TestAttack(); });
 }
 
 DefaultEnemy::~DefaultEnemy()
@@ -30,12 +34,15 @@ DefaultEnemy::~DefaultEnemy()
 void DefaultEnemy::Update(float deltaTime)
 {
     Enemy::Update(deltaTime);
+    //基底クラスの処理を実行
+    MoveAttackCollide();
+    if(GetCoolTime() <= 0)
+    damageCoolTime_ -= deltaTime / 60.0f;
 }
 
 void DefaultEnemy::LateUpdate(float deltaTime)
 {
     Enemy::LateUpdate(deltaTime);
-
 }
 
 void DefaultEnemy::Draw() const
@@ -45,9 +52,30 @@ void DefaultEnemy::Draw() const
 
 void DefaultEnemy::React(Actor& other)
 {
+    if (other.GetName() == ActorName::Player && CurrentState() != EnemyState::Damage) {
+        ChangeState(EnemyState::Attack);
+    }
 }
 
+void DefaultEnemy::TakeDamage(float damage)
+{
+    //死亡しているならreturn
+    if (CurrentState() == EnemyState::Dead) return;
+
+    hp_ -= damage;
+    //hpが0なら死亡
+    if(IsDying()) ChangeState(EnemyState::Dead);
+    else ChangeState(EnemyState::Damage);
+}
 
 void DefaultEnemy::Debug(float deltaTime)
 {
+    ImGui::Begin("Enemystatus");
+    ImGui::Value("hp",hp_);
+    ImGui::End();
+}
+
+void DefaultEnemy::TestAttack()
+{
+    attackCollider_->IsAttack(0.5f, 10);
 }
--- MyGame/Src/Actor/Enemy/Enemy.cpp
+++ MyGame/Src/Actor/Enemy/Enemy.cpp
@@ -2,6 +2,7 @@
 #include <imgui/imgui.h>
 #include "../../World/IWorld.h"
 #include "../../GameSystem/InputSystem/InputSystem.h"
+#include "../../Actor/Player/Player.h"
 
 #include "State/EnemyState.h"
 #include "State/EnemyIdle.h"
@@ -10,17 +11,22 @@
 Enemy::Enemy(IWorld* world, const GSvector3& position, GSuint mesh)
     :Charactor(world,position,mesh)
 {
+    player_ = world_->GetCharactor("Player");
+    tag_ = ActorName::Enemy;
+    colliderOffset_ = { 0.0f,1.0f,0.0f };
+    attackCollider_ = new AttackCollide(this,0.75f,{ 0,0,0 },colliderOffset_);
+    world_->AddActor(attackCollider_);
 }
 
 
 Enemy::~Enemy()
 {
+    attackCollider_->Die();
 }
 
 void Enemy::Update(float deltaTime)
 {
     Charactor::Update(deltaTime);
-    collider_.Position(transform_.position() + colliderOffset_);
 }
 
 void Enemy::LateUpdate(float deltaTime)
@@ -37,10 +43,43 @@ void Enemy::Draw() const
 
 void Enemy::React(Actor& other)
 {
+
 }
 
 void Enemy::MovePosition(float deltaTime)
 {
+    //プレイヤーいない・もしくは死んでいるならreturn
+    if (!player_ || player_->IsDying()) return;
+    // ターゲット方向のベクトルを求める
+    GSvector3 target = player_->Transform().position() - transform_.position();
+    // 前向き方向のベクトルを取得
+    GSvector3 forward = transform_.forward();
+    // ベクトルのy成分を無効にする
+    forward.y = 0.0f;
+    target.y = 0.0f;
+    // 前向き方向のベクトルとターゲット方向のベクトルの角度差を求める
+    float angle = GSvector3::signedAngle(forward, target);
+    // ターゲット方向を向く
+    transform_.rotate(0.0f, angle * deltaTime, 0.0f);
+    // 前進する（ローカル座標基準）
+    transform_.translate(0.0f, 0.0f, 0.1f * deltaTime);
+}
+
+void Enemy::MoveAttackCollide()
+{
+    GSvector3 forward = transform_.forward() * 0.8f;
+    //攻撃判定を追従
+    attackCollider_->Transform().position(transform_.position() + forward);
+}
+
+void Enemy::SetCoolTime(float time)
+{
+    damageCoolTime_ = time;
+}
+
+float Enemy::GetCoolTime()
+{
+    return damageCoolTime_;
 }
 
 

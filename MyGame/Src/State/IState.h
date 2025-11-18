413cf2231552706eb37f2f4120eafa97c0b1ad3c
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-11-18 16:12:41 +0900

    Update:ステート取得メゾットを共通化

--- MyGame/Src/State/IState.h
+++ MyGame/Src/State/IState.h
@@ -7,8 +7,8 @@ public:
 	virtual void Update(float deltaTime) = 0;
 	virtual void Enter() = 0;
 	virtual void Exit() = 0;
-	virtual void SetID(int id) = 0;
-	virtual int GetID() = 0;
+    virtual void SetID(int id) { stateID = id; };
+    virtual int GetID() { return stateID; };
 protected:
 	int stateID{ 0 };
 };

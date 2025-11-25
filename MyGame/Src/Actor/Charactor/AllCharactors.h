2869d7a7e2459a63e4c937a019637d019b940b55
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-11-25 11:07:29 +0900

    Add:全キャラヘッダー読み込みヘッダー追加

---
+++ MyGame/Src/Actor/Charactor/AllCharactors.h
@@ -0,0 +1,10 @@
+﻿#ifndef ALL_CHARACTORS_H_
+#define ALL_CHARACTORS_H_
+//=========全キャラヘッター取得=========
+#include "Charactor.h"
+#include "Player/Player.h"
+#include "Enemy/Enemy.h"
+#include "Enemy/SwordEnemy1/SwordEnemy1.h"
+#include "Enemy/Boss/Boss.h"
+
+#endif

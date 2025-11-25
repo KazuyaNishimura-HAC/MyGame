d43d6fefda632034bd6dda30b32b6e94a8a767e2
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-11-25 11:06:20 +0900

    Add:扉解放イベント実装

---
+++ MyGame/Src/GameSystem/Event/OpenDoorEvent.cpp
@@ -0,0 +1,32 @@
+﻿#include "OpenDoorEvent.h"
+
+OpenDoorEvent::OpenDoorEvent(IWorld* world, int invokeType,int eventNum, const GSvector3& position, GSvector2 size)
+    :Event::Event(world, invokeType, position, size)
+{
+    world_ = world;
+    eventNumber_ = eventNum;
+}
+
+void OpenDoorEvent::Update(float deltaTime)
+{
+    if (!world_->GetTimeLine().IsRunning()) {
+        EndEvent();
+    }
+}
+
+void OpenDoorEvent::Draw() const
+{
+}
+
+void OpenDoorEvent::BeginEvent()
+{
+    std::string eventName = "OpenDoor" + std::to_string(eventNumber_);
+    world_->GetTimeLine().StartTimeLine(eventName);
+}
+
+void OpenDoorEvent::EndEvent()
+{
+    SetInvoke(false);
+    //一度きりで終了（完全削除）
+    isEnd_ = true;
+}
---
+++ MyGame/Src/GameSystem/Event/OpenDoorEvent.h
@@ -0,0 +1,21 @@
+﻿#ifndef OPEN_DOOR_EVENT_H_
+#define OPEN_DOOR_EVENT_H_
+#include "Event.h"
+
+class GUI;
+
+class OpenDoorEvent : public Event
+{
+public:
+    OpenDoorEvent(IWorld* world, int invokeType, int eventNum = 0, const GSvector3& position = { 0,0,0 }, GSvector2 size = { 1,1 });
+    void Update(float deltaTime) override;
+    void Draw()const override;
+private:
+    void BeginEvent()override;
+    void EndEvent()override;
+
+    Player* player_{ nullptr };
+    int eventNumber_{ 0 };
+};
+
+#endif

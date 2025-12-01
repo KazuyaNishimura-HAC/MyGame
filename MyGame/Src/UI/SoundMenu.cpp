423992fed0d2cd18f8e88ff5a5400f21f85afc49
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-12-01 15:40:59 +0900

    Add:サウンド設定画面追加

---
+++ MyGame/Src/UI/SoundMenu.cpp
@@ -0,0 +1,51 @@
+﻿#include "SoundMenu.h"
+#include "GUI.h"
+#include "ButtonList/VerticalButtonList.h"
+#include "ButtonList/Button.h"
+#include "ButtonList/ButtonSlider.h"
+#include "../Sound/SoundManager.h"
+#include "../Graphics/Effect/PostEffect.h"
+
+SoundMenu::SoundMenu()
+{
+    SoundSettings();
+}
+
+void SoundMenu::Draw()const
+{
+    if (!enable_)return;
+    guiManager_.Draw();
+}
+
+void SoundMenu::Update(float deltaTime)
+{
+    if (!enable_)return;
+    guiManager_.Update(deltaTime, deltaTime);
+}
+
+void SoundMenu::SoundSettings()
+{
+    soundMenu_ = new VerticalButtonList({ Screen::HalfWidth - 100, 256 + 100 }, 5, 100);
+    soundMenu_->AddButton(new ButtonSlider(Texture::SoundAll, [](float v) {SoundManager::SetMasterVolume(v); }, SoundManager::GetMasterVolume(), 0.1f, 0, 1));
+    soundMenu_->AddButton(new ButtonSlider(Texture::SoundBGM, [](float v) {SoundManager::SetBGMVolume(v); }, SoundManager::GetBGMVolume(), 0.1f, 0, 1));
+    soundMenu_->AddButton(new ButtonSlider(Texture::SoundSE, [](float v) {SoundManager::SetAllSEVolume(v); }, SoundManager::GetSEAllVolume(), 0.1f, 0, 1));
+    guiManager_.AddGUI(soundMenu_);
+    enable_ = false;
+}
+
+void SoundMenu::BeginSoundSetting()
+{
+    enable_ = true;
+    PostEffect::Instance().IsBlur(true);
+}
+
+void SoundMenu::EndSoundSetting()
+{
+    enable_ = false;
+    PostEffect::Instance().IsBlur(false);
+}
+
+bool SoundMenu::GameEnd()
+{
+    return gameEnd_;
+}
---
+++ MyGame/Src/UI/SoundMenu.h
@@ -0,0 +1,26 @@
+﻿#ifndef SOUND_MENU_H_
+#define SOUND_MENU_H_
+
+#include "Image.h"
+#include "GUI.h"
+#include "GUIManager.h"
+
+class GUI;
+class VerticalButtonList;
+
+class SoundMenu : public GUI
+{
+public:
+    SoundMenu();
+    void Draw()const override;
+    void Update(float deltaTime)override;
+    void BeginSoundSetting();
+    void EndSoundSetting();
+    bool GameEnd();
+private:
+    void SoundSettings();
+    VerticalButtonList* soundMenu_;
+    GUIManager guiManager_;
+    bool gameEnd_{ false };
+};
+#endif

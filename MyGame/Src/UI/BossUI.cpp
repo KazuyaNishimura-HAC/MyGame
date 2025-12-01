97f32782581d9a502d24456627b7ae2c763c3554
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-12-01 15:40:43 +0900

    Add:ボス専用UI追加

---
+++ MyGame/Src/UI/BossUI.cpp
@@ -0,0 +1,39 @@
+﻿#include "BossUI.h"
+#include"../Actor/Charactor/Enemy/Boss/Boss.h"
+#include "../GameSystem/InputSystem/InputSystem.h"
+#include "HorizontalGauge.h"
+#include "Texture.h"
+#include "../World/IWorld.h"
+BossUI::BossUI(IWorld* world, Boss* boss, const GSvector2& position)
+    :GUI(position)
+{
+    world_ = world;
+    boss_ = boss;
+    healthGauge_ = new HorizontalGauge(
+        position + GSvector2{Screen::HalfWidth, 100.0f},
+        { 3.0f,1.0f },
+        Texture::Data(Texture::BossSliderFill, { 0.5f,0.5f }),
+        Texture::Data(Texture::BossMenuSliderBG, { 0.5f,0.5f })
+    );
+}
+
+BossUI::~BossUI()
+{
+    delete healthGauge_;
+    healthGauge_ = nullptr;
+}
+
+void BossUI::Update(float deltaTime)
+{
+    if (boss_->IsBattleMode()) {
+        bool flg = boss_->IsBattleMode();
+    }
+    float health = boss_->GetCurrentHealth() / boss_->GetMaxHealth();
+    healthGauge_->FillAmount(health);
+}
+
+void BossUI::Draw() const
+{
+    if (!enable_ || !boss_->IsBattleMode()) return;
+    healthGauge_->Draw();
+}
---
+++ MyGame/Src/UI/BossUI.h
@@ -0,0 +1,18 @@
+﻿#ifndef BOSS_UI_H_
+#define BOSS_UI_H_
+#include "GUI.h"
+class HorizontalGauge;
+class Boss;
+
+class BossUI : public GUI {
+public:
+    BossUI(IWorld* world, Boss* boss, const GSvector2& position = { 0,0 });
+    ~BossUI();
+    void Update(float deltaTime)override;
+    void Draw()const override;
+private:
+    IWorld* world_{ nullptr };
+    HorizontalGauge* healthGauge_{ nullptr };
+    Boss* boss_{ nullptr };
+};
+#endif

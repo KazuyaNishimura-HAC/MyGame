47d82159d31a4daad884034b61e82faa807622b8
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-12-16 10:30:06 +0900

    Update:敵ステータス変更

--- MyGame/Src/GameSystem/BattleSystem/BattleManager.cpp
+++ MyGame/Src/GameSystem/BattleSystem/BattleManager.cpp
@@ -23,7 +23,7 @@ void BattleManager::Update(float deltaTime)
 
 void BattleManager::SpawnPlayer()
 {
-    Player* player = new Player(world_, { 0,0,0 }, { 0,90,0 }, Status{ 100, 10 });
+    Player* player = new Player(world_, { 0,0,0 }, { 0,90,0 }, Status{ 100, 10, 3 });
     player->Init(this);
     world_->AddPlayer(player);
 }
@@ -33,17 +33,17 @@ void BattleManager::SpawnEnemis()
     //グループ数分要素を確保
     groupEnemis_.resize(enemyGroupCount_ + 1);
     groupCount_.resize(enemyGroupCount_ + 1);
-    groupEnemis_[1].push_back(new SwordEnemy1(world_, 1, { 35,0,0 },{ 0,270,0 }));
-    groupEnemis_[2].push_back(new SwordEnemy1(world_, 2, { 85,0,-5 }, { 0,270,0 }));
-    groupEnemis_[2].push_back(new SwordEnemy1(world_, 2, { 85,0,5 }, { 0,270,0 }));
-    groupEnemis_[3].push_back(new SwordEnemy1(world_, 3, { 130,0,-5 }, { 0,270,0 }));
-    groupEnemis_[3].push_back(new SwordEnemy1(world_, 3, { 130,0,0 }, { 0,270,0 }));
-    groupEnemis_[3].push_back(new SwordEnemy1(world_, 3, { 130,0,5 }, { 0,270,0 }));
-    groupEnemis_[4].push_back(new SwordEnemy1(world_, 4, { 180,0,-5 }, { 0,270,0 }, Status{ 120, 5 }));
-    groupEnemis_[4].push_back(new SwordEnemy1(world_, 4, { 180,0,5 }, { 0,270,0 }, Status{ 120, 5 }));
-    groupEnemis_[4].push_back(new SwordEnemy1(world_, 4, { 180,0,-10 }, { 0,270,0 }, Status{ 120, 5 }));
-    groupEnemis_[4].push_back(new SwordEnemy1(world_, 4, { 180,0,10 }, { 0,270,0 }, Status{ 120, 5 }));
-    boss_ = new Boss(world_, 4, { 185,40,0 }, { 0,270,0 }, Status{ 500, 30 });
+    groupEnemis_[1].push_back(new SwordEnemy1(world_, 1, { 35,0,0 },{ 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[2].push_back(new SwordEnemy1(world_, 2, { 85,0,-5 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[2].push_back(new SwordEnemy1(world_, 2, { 85,0,5 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[3].push_back(new SwordEnemy1(world_, 3, { 130,0,-5 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[3].push_back(new SwordEnemy1(world_, 3, { 130,0,0 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[3].push_back(new SwordEnemy1(world_, 3, { 130,0,5 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[4].push_back(new SwordEnemy1(world_, 4, { 180,0,-5 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[4].push_back(new SwordEnemy1(world_, 4, { 180,0,5 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[4].push_back(new SwordEnemy1(world_, 4, { 180,0,-10 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    groupEnemis_[4].push_back(new SwordEnemy1(world_, 4, { 180,0,10 }, { 0,270,0 }, Status{ 150, 5, 0 }));
+    boss_ = new Boss(world_, 4, { 185,40,0 }, { 0,270,0 }, Status{ 1200, 15, 15 });
     groupEnemis_[4].push_back(boss_);
     for (int i = 0; i < enemyGroupCount_ + 1;++i) {
         groupCount_[i] = groupEnemis_[i].size();
@@ -86,6 +86,31 @@ void BattleManager::BossDeadMessage()
 bool BattleManager::IsBossDead() const
 {
     return bossDead_;
+}
+
+void BattleManager::PlayerDeadMessage()
+{
+    playerDead_ = true;
+}
+
+bool BattleManager::IsPlayerDead() const
+{
+    return playerDead_;
+}
+
+bool BattleManager::GameEnd() const
+{
+    return playerDead_ || bossDead_;
+}
+
+void BattleManager::SetResultData(ResultData data)
+{
+    resultData_ = data;
+}
+
+ResultData BattleManager::GetResultData() const
+{
+    return resultData_;
 }
 
 void BattleManager::BeginEvent(int group)
--- MyGame/Src/GameSystem/BattleSystem/BattleManager.h
+++ MyGame/Src/GameSystem/BattleSystem/BattleManager.h
@@ -6,6 +6,14 @@ class Enemy;
 class Event;
 class Boss;
 
+struct ResultData {
+    ResultData(int maxCombo, int parryCount, int totalDamage, bool playerDead) :maxCombo{ maxCombo }, parryCount{ parryCount }, totalDamage{ totalDamage }, playerDead{ playerDead } {};
+    ResultData() : ResultData(0, 0, 0,false) {};
+    int maxCombo = 0;
+    int parryCount = 0;
+    int totalDamage = 0;
+    bool playerDead = false;
+};
 class BattleManager
 {
 public:
@@ -15,6 +23,11 @@ public:
     void EnemyDeadMessage(int group);
     void BossDeadMessage();
     bool IsBossDead() const;
+    void PlayerDeadMessage();
+    bool IsPlayerDead() const;
+    bool GameEnd() const;
+    void SetResultData(ResultData data);
+    ResultData GetResultData()const ;
 private:
     void SpawnPlayer();
     void SpawnEnemis();
@@ -28,6 +41,8 @@ private:
     const int enemyGroupCount_{ 4 };
     int battleEventCount_{ 0 };
     bool bossDead_{ false };
+    bool playerDead_{ false };
+    ResultData resultData_{};
 };
 
 #endif

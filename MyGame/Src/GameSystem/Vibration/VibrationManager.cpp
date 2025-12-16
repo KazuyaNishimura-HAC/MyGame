21711acdd151ac3df91909672dee2de6deb0636a
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-12-16 10:36:42 +0900

    Add:コントローラー振動処理の追加

---
+++ MyGame/Src/GameSystem/Vibration/VibrationManager.cpp
@@ -0,0 +1,47 @@
+﻿#include "VibrationManager.h"
+
+VibrationManager::Vibration left_, right_;
+VibrationManager::VibrationManager()
+{
+}
+
+void VibrationManager::Update(float deltaTime)
+{
+    gsXBoxPadSetVibration(0, left_.Amount(), right_.Amount());
+    left_.Update(deltaTime);
+    right_.Update(deltaTime);
+}
+
+void VibrationManager::SetVibration(Vibration left, Vibration right)
+{
+    SetVibrationLeft(left);
+    SetVibrationRight(right);
+}
+
+void VibrationManager::SetVibrationLeft(Vibration left)
+{
+    left_ = left;
+}
+
+void VibrationManager::SetVibrationRight(Vibration right)
+{
+    right_ = right;
+}
+
+void VibrationManager::Stop()
+{
+    gsXBoxPadSetVibration(0, 0, 0);
+}
+
+void VibrationManager::Vibration::Update(float deltaTime)
+{
+    //無限
+    if (duration == 0)return;
+    timer = MAX(timer - deltaTime, 0);
+}
+
+float VibrationManager::Vibration::Amount()const
+{
+    if (duration == 0)return 0;
+    return LERP(timer / duration, end, begin);
+}
---
+++ MyGame/Src/GameSystem/Vibration/VibrationManager.h
@@ -0,0 +1,30 @@
+﻿#ifndef VIBRATION_MANAGER_H_
+#define VIBRATION_MANAGER_H_
+
+#include <gslib.h>
+
+class VibrationManager
+{
+public:
+    struct Vibration
+    {
+        Vibration(float begin = 0, float duration = 0, float end = 0)
+            :begin{ begin }, duration{ duration }, timer{ duration }, end{ end } {};
+        void Update(float deltaTime);
+        float Amount()const;
+    private:
+        float duration{ 0 };
+        float begin{ 0 };
+        float timer{ 0 };
+        float end{ 0 };
+    };
+
+    VibrationManager();
+    void Update(float deltaTime);
+    static void SetVibration(Vibration left, Vibration right);
+    static void SetVibrationLeft(Vibration left);
+    static void SetVibrationRight(Vibration right);
+    static void Stop();
+};
+
+#endif

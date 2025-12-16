83c47ea997ad12adbb5042b974076dd904050911
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2025-12-16 10:38:03 +0900

    add：数字画像表示UIの実装

---
+++ MyGame/Src/UI/ValueText.cpp
@@ -0,0 +1,54 @@
+﻿#include "ValueText.h"
+
+ValueText::ValueText(GSvector2 position,GSvector2 size, GScolor color, int value, float align)
+    :GUI(position, size, color), value_{ value }
+{
+    UpdateValue(value, align);
+}
+
+void ValueText::Update(float deltaTime)
+{
+}
+
+void ValueText::Draw() const
+{
+    if (!enable_)return;
+    //fill数値分０を追加
+    for (int i = 1; i < fill_ + 1; i++) {
+        if (value_ < pow(10, i)) DrawNumber(0, i);
+    }
+
+    for (int i = digit_ - 1; i >= 0; i--)
+    {
+        int number = value_ / (int)powf(10, i);
+        number %= (int)powf(10, i + 1);
+        DrawNumber(number, i);
+    }
+}
+
+void ValueText::UpdateValue(int value, float align)
+{
+    value_ = value;
+    if (value == 0)digit_ = 1;
+    else digit_ = log10f(value) + 1;
+    //左揃え0右揃え1
+    align_ = digit_ * (1 - align);
+}
+
+void ValueText::DrawNumber(int num, float digit)const
+{
+    const float constNumberWidth = numbers_.size_.x / 10;
+    GSvector2 position = position_;
+    position.x -= (digit - align_) * constNumberWidth * size_.x;
+    GSrect rect{ num * constNumberWidth,0,(num + 1) * constNumberWidth ,numbers_.size_.y };
+    gsDrawSprite2D(numbers_.name_, &position, &rect, NULL, &color_, &size_, 0);
+}
+
+void ValueText::NumberFill(int num) {
+    fill_ = num;
+}
+
+GSuint ValueText::Texture() const
+{
+    return numbers_.name_;
+}
---
+++ MyGame/Src/UI/ValueText.h
@@ -0,0 +1,28 @@
+﻿#ifndef VALUE_TEXT_H_
+#define VALUE_TEXT_H_
+
+#include "GUI.h"
+#include "Texture.h"
+
+class ValueText :public GUI
+{
+public:
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="align">左揃え0, 右揃え1</param>
+    ValueText(GSvector2 position,GSvector2 size = { 1,1 }, GScolor color = { 1, 1, 1, 1 }, int value = 0, float align = 1);
+    virtual void Update(float deltaTime) override;
+    virtual void Draw()const override;
+    void UpdateValue(int value, float align = 1);
+    void NumberFill(int num = 0);
+    virtual GSuint Texture()const override;
+protected:
+    void DrawNumber(int num, float digit)const;
+    int value_, digit_;
+    int  fill_{ 0 };
+    Texture::Data numbers_{ Texture::NumberText };
+    float align_;
+};
+
+#endif

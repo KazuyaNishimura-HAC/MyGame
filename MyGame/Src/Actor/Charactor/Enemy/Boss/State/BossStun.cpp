6243e13cd5c980e1d79f019fc597bebe584af6d6
Nishimura Kazuya <g023g1024@g.neec.ac.jp>
2026-02-06 09:42:20 +0900

    Add：ボス待機ステートの実装

---
+++ MyGame/Src/Actor/Charactor/Enemy/Boss/State/BossStun.cpp
@@ -0,0 +1,37 @@
+﻿#include "BossStun.h"
+#include "../../../../../World/IWorld.h"
+#include "../../../../../GameSystem/InputSystem/InputSystem.h"
+BossStun::BossStun(Boss* owner)
+    :BossState::BossState(owner)
+{
+}
+
+void BossStun::Update(float deltaTime)
+{
+    stunTimer_ += deltaTime / 60.0f;
+    if (stunTimer_ >= stunDuration_) owner_->ChangeState(Idle);
+    float currentBreakPt = owner_->CurrentBreakPoint();
+    float maxBrekPt = owner_->MaxBreakPoint();
+    float breakPt = currentBreakPt - (maxBrekPt * deltaTime / 60.0f) / stunDuration_;
+    owner_->SetBreakPoint(breakPt);
+}
+
+void BossStun::Enter()
+{
+    stunTimer_ = 0.0f;
+    //エフェクト再生
+    effectHandle_ = Effect::CreateHandle(Effect::Confusion);
+    EffectParam param;
+    param.position = owner_->Transform().position() + GSvector3{ 0,3.0f,0 };
+    Effect::SetParam(effectHandle_, param);
+    owner_->ChangeMotion(BossMotion::Guard);
+    owner_->SetStun(true);
+}
+
+void BossStun::Exit()
+{
+    Effect::Stop(effectHandle_);
+    owner_->SetStun(false);
+    //ブレイク値をリセット
+    owner_->ResetBreakPoint();
+}
---
+++ MyGame/Src/Actor/Charactor/Enemy/Boss/State/BossStun.h
@@ -0,0 +1,18 @@
+﻿#ifndef BOSS_STUN_H_
+#define BOSS_STUN_H_
+#include "BossState.h"
+
+class BossStun : public BossState
+{
+public:
+    BossStun(Boss* owner);
+    void Update(float deltaTime)override;
+    void Enter()override;
+    void Exit()override;
+private:
+    float stunTimer_{ 0.0f };
+    const float stunDuration_{ 5.0f };
+    GSuint effectHandle_;
+};
+
+#endif
